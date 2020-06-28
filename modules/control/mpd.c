cc31e8383509f4c8107870d1ad3e4b43886affb8
knightwayne <arnav7ishaan@gmail.com>
2020-06-28 10:29:53 +0530

    select() added

--- modules/control/mpd.c
+++ modules/control/mpd.c
@@ -8,19 +8,16 @@
 #include <vlc_plugin.h>
 #include <vlc_interface.h>
 /*Added during MPD Development*/
+#include <sys/select.h>
 #include <vlc_fs.h>
 #include <vlc_network.h>
-
 
 /*Port and Address to look up at Request at - http://192.168.0.101:6600*/
 #define PORT 6600
 
-/*<---------------------------Core MPD Server----------------------------->*/
-
 //System Variables Declaration Here.
 struct intf_sys_t
 {
-    //xxxxx
     int serverFD;
     vlc_thread_t th;
 };
@@ -30,8 +27,9 @@ void *thread_func(void *arg) //Parsing Polling Listening to Requests
     intf_thread_t *intfa = (intf_thread_t *)arg;
     msg_Info(intfa, "Hello from thread side!");
     int server_fd=intfa->p_sys->serverFD;
+    //msg_Info(intfa, "%i serverFD2\n", server_fd);
 
-    //thread work;
+    // Thread Handling
     int client_fd;
     struct sockaddr_in clientAddr;
     int clientAddrLen = sizeof(clientAddr);
@@ -43,52 +41,77 @@ void *thread_func(void *arg) //Parsing Polling Listening to Requests
         exit(EXIT_FAILURE);
     }
 
-    //Select case handling-------------------------------------------------------
+    // Select() for handling multiple clients - polling method
+    fd_set currentSockets,readySockets;
+    FD_ZERO(&currentSockets);
+    FD_SET(server_fd,&currentSockets);  //adding server_fd to currentSockets
 
-    // New Connection Comes, Handling It
-    if ((client_fd = accept(server_fd, (struct sockaddr *)&clientAddr, (socklen_t *)&clientAddrLen)) < 0)
+    while(true) //event loop
     {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-
-
-
-    //Select case handling---
-
-    //Parsing
-    char *vlc = "dummy"; //dummy valid command like play pause
-
-    while (1) //is this (infinite while loop) the correct way to keep listensing requests in the background??
-    {
-        int n;
-        char buffer[256];
-
-        //Recieve
-        bzero(buffer, 256);
-        n = read(client_fd, buffer, 255); //client_fd to be passed as argument in intf;
-        if (n < 0)
-            printf("ERROR reading from socket");
-        else
+        readySockets=currentSockets;
+        if(select(FD_SETSIZE, &readySockets,NULL,NULL,NULL)<0)
         {
-            printf("%d Buffer %s %c\n", strlen(buffer), buffer, buffer[2]);
+            perror("select");
+            exit(EXIT_FAILURE);
         }
+        //msg_Info(intfa, "Enter for loop\n");
 
-        //Action & Send
-        if (!strcmp(buffer, vlc)) //one of the valid commands--MAIN ACTION AND PLAYBACK CONTROL HERE
+        for (int i = 0; i < FD_SETSIZE; i++)
         {
-            //send(client_fd, ok, strlen(ok), 0);
-            printf("OK vlc opening\n");
-            system(buffer);
-        }
-        else //not one of the valid commands
-        {
-            //send(client_fd, nok, strlen(nok), 0);
-            printf("Not OK Terminating\n");
-            break;
-        }
-    }
+            if(FD_ISSET(i,&readySockets))
+            {
+                if(i==server_fd)// read instructions at server
+                {
+                    // New Connection Comes, Handling It
+                    if ((client_fd = accept(server_fd, (struct sockaddr *)&clientAddr, (socklen_t *)&clientAddrLen)) < 0)
+                    {
+                        perror("accept");
+                        exit(EXIT_FAILURE);
+                    }
+                    FD_SET(client_fd,&currentSockets);
+                }   
+                else
+                {
+                    //handle connection
+                    msg_Info(intfa, "client_fd %d\n", i);
 
+                    int n;
+                    const char *vlc="command";
+                    const char *ok="ack", *nok="list";
+                    char buffer[256];
+                    //Recieve
+                    bzero(buffer, 256);
+                    client_fd=i;
+                    if((n = read(client_fd, buffer, 255))<0) //client_fd to be passed as argument in intf;
+                    {
+                        perror("read");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    printf("BufferSize: %ld \tBuffer %s \n", strlen(buffer), buffer);
+                    msg_Info(intfa,"BufferSize: %ld \tBuffer %s \n", strlen(buffer), buffer);
+
+                    //Action & Send
+                    //Main Parsing Function
+                    if (!strcmp(buffer, vlc)) //one of the valid commands--MAIN ACTION AND PLAYBACK CONTROL HERE
+                    {
+                        send(client_fd, ok, strlen(ok), 0);
+                        printf("OK Command Read\n");
+                        bzero(buffer, 256);
+                    }
+                    else //not one of the valid commands
+                    {
+                        send(client_fd, nok, strlen(nok), 0);
+                        printf("Unacceptable Command\n");
+                        FD_CLR(i, &currentSockets);
+                    }
+                    //FD_CLR(i,&currentSockets);
+                }
+            }
+        }
+        //break;
+    }//while
+    msg_Info(intfa, "Exit while loop\n");
     return NULL;
 }
 
@@ -143,8 +166,9 @@ static int Open(vlc_object_t *obj)
     sys->serverFD=server_fd;
     sys->th=mpd_t;
 
+    //msg_Info(intfa, "%i serverFD1\n", server_fd);
     int i = vlc_clone(&sys->th, thread_func, intfa, 0);
-    msg_Info(intfa, "Thread Started from Open() %d!", i);
+    //msg_Info(intfa, "Thread Started from Open() %d!", i);
 
     return VLC_SUCCESS;
     //error case -> add go to statements if needed
@@ -160,7 +184,7 @@ static void Close(vlc_object_t *obj)
     msg_Info(intf, "Good bye MPD Server");
     
     //free(&sys->th);
-    free(sys->serverFD);
+    //free(sys->serverFD);
     free(sys);
 }
 
