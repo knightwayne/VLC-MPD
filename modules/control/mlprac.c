896a578ee092d716a470c127eb994d17e83d4c7a
knightwayne <arnav7ishaan@gmail.com>
2020-07-22 04:58:49 +0530

    medialibrary practice file

--- modules/control/mlprac.c
+++ modules/control/mlprac.c
@@ -43,9 +43,7 @@ static int Open(vlc_object_t *obj)
     // 1. Testing MediaLibrary
     //1
     int c=0;
-    vlc_object_t *newObj1 = vlc_object_create(obj,8000);
-    vlc_medialibrary_t* p_ml=vlc_ml_instance_get(newObj1);
-    //msg_Info(intf, "f0 %ld %ld",sizeof(newObj1),sizeof(p_ml));
+    vlc_medialibrary_t* p_ml=vlc_ml_instance_get(obj);
 
     //2
     vlc_ml_query_params_t params = vlc_ml_query_params_create();
@@ -53,27 +51,55 @@ static int Open(vlc_object_t *obj)
     //p_params->psz_pattern="Natu";
     //msg_Info(intf, "f0 %ld %ld %s",sizeof(params),sizeof(p_params),p_params->psz_pattern);
     
-    //3
-    c=7;
-    c=vlc_ml_add_folder (p_ml, "/home/nightwayne/Music/");
-    msg_Info(intf, "f0 %d",c);
+    // //3
+    // c=vlc_ml_add_folder (p_ml, "/home/nightwayne/Music/");
+    // msg_Info(intf,"f0 %d",c);
 
-    //4
-    void* p_get = vlc_ml_get(p_ml,VLC_ML_GET_MEDIA_BY_MRL,"/home/nightwayne/Music/Natural.mp3");
-    vlc_ml_media_t *p_mlmt = (vlc_ml_media_t*)p_get;
-    msg_Info(intf, "f0 %d",c);
+    // //4
+    // void* p_get = vlc_ml_get(p_ml,VLC_ML_GET_MEDIA_BY_MRL,"/home/nightwayne/Music/Natural.mp3");
+    // vlc_ml_media_t *p_mlmt = (vlc_ml_media_t*)p_get;
+    // msg_Info(intf,"f0 %d",c);
+    // //msg_Info(intf,"%d",p_mlmt->i_id);
 
-    //5
-    c = vlc_ml_control(p_ml,VLC_ML_ADD_FOLDER,"/home/nightwayne/Music/");
-    msg_Info(intf, "f0 %d",c);
+    // //5
+    // c = vlc_ml_control(p_ml,VLC_ML_ADD_FOLDER,"/home/nightwayne/Music/");
+    // msg_Info(intf,"f0 %d",c);
 
-    //6
-    c = vlc_ml_list(p_ml,VLC_ML_GET_MEDIA_BY_MRL,VLC_ML_LIST_AUDIOS,p_params/* ,vlc_ml_media_list_t** */);
-    msg_Info(intf, "f0 %d",c);
+    // //6
+    // c = vlc_ml_list(p_ml,VLC_ML_GET_MEDIA_BY_MRL,VLC_ML_LIST_AUDIOS,p_params/* ,vlc_ml_media_list_t** */);
+    // msg_Info(intf,"f0 %d",c);
 
+    //7
+    vlc_ml_media_list_t *aa =	vlc_ml_list_audio_media (p_ml, p_params);
+    msg_Info(intf,"%d",aa->i_nb_items);
+    vlc_ml_media_t *ans=&aa->p_items[1];
+    int id=ans->i_id;
+    msg_Info(intf,"%s %d",ans->psz_title,id);
+
+    //8
+    vlc_ml_artist_list_t * ab =vlc_ml_list_artists (p_ml, p_params, true);
+    msg_Info(intf,"%d",ab->i_nb_items);
+    vlc_ml_artist_t *anb;
+    for(int i=0;i<ab->i_nb_items;i++)
+    {
+        anb=&ab->p_items[i];
+        int id2=anb->i_id;
+        msg_Info(intf,"%s %d",anb->psz_name,id2);
+    }
+
+    // pointer free error - why?
+    // vlc_ml_media_release (ans);
+    // msg_Info(intf,"f1 %d",c);
+    // vlc_ml_media_list_release (aa);
+    // msg_Info(intf,"f2 %d",c);
+    // vlc_ml_artist_release (anb);
+    // msg_Info(intf,"f3 %d",c);
+    // vlc_ml_artist_list_release (ab);
+    // msg_Info(intf,"f4 %d",c);
+    
+    //double free or corruption (out) Aborted (core dumped)
 
     // 2. Module End
-    vlc_object_delete(newObj1);
     return VLC_SUCCESS;
     //error case -> add go to statements if needed
 }
